0a183672658cef261a5460e5780c6a6307bf2e24
Ole J. Skogstad <skogstad@protonmail.com>
2019-07-24 02:06:48 +0200

    Add JsonFormatters

--- Startup.cs
+++ Startup.cs
@@ -19,12 +19,14 @@ namespace foo_chess_server
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvcCore();
-            services.AddDbContext<FooChessContext>(
+            services
+                .AddDbContext<FooChessContext>(
                 options => 
                 { 
                     options.UseNpgsql(Configuration.GetConnectionString("DefaultConnection"));
-                });
+                })
+                .AddMvcCore()
+                .AddJsonFormatters();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
