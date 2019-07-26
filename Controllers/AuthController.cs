db938321cd08b8ed3d2220a8599437b2d5d385f3
Ole J. Skogstad <skogstad@protonmail.com>
2019-07-26 23:40:35 +0200

    Create login endpoint

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -1,5 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using foo_chess_server.Database;
 
 namespace foo_chess_server.Controllers
 {
@@ -7,11 +9,28 @@ namespace foo_chess_server.Controllers
     [Route("api/auth")]
     public class AuthController : ControllerBase
     {
-        [HttpGet]
-        public async Task<IActionResult> Get() 
+       private FooChessContext _fooChessContext;
+        public AuthController(FooChessContext fooChessContext) 
         {
-            await Task.FromResult(1);
-            return Ok("OK from AuthController");
+            _fooChessContext = fooChessContext;
+        }
+
+        [HttpPost("login/")]
+        public async Task<IActionResult> LogIn([FromBody] TokenRequest request) 
+        {
+            var user = await _fooChessContext.Users.SingleOrDefaultAsync(usr => usr.Email.Equals(request.Email));
+            if(user == null)
+            {
+                return Unauthorized("Incorrect email and/or password");
+            }
+
+            return Ok("User found");
+        }
+
+        public class TokenRequest
+        {
+            public string Email { get; set; }
+            public string Password { get; set; }
         }
     }
 }
