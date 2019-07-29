d1925cd605677138ae1aba45e56ec3606f2fa5ba
Ole J. Skogstad <skogstad@protonmail.com>
2019-07-29 18:47:04 +0200

    Create TournamentsController

---
+++ Controllers/TournamentsController.cs
@@ -0,0 +1,45 @@
+using System.Threading.Tasks;
+using Isolani.Model;
+using Isolani.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Isolani.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TournamentsController : ControllerBase
+    {
+        private readonly ITournamentService _tournamentService;
+
+        public TournamentsController(ITournamentService tournamentService)
+        {
+            _tournamentService = tournamentService;
+        }
+
+        [AllowAnonymous]
+        [HttpGet("public/")]
+        public async Task<IActionResult> GetAllPublicTournaments()
+        {
+            var allTournaments = await _tournamentService.GetAllPublicTournamentsAsync();
+            return Ok(allTournaments);
+        }
+
+        #if DEBUG
+        [AllowAnonymous]
+        #endif
+        [HttpPost]
+        public async Task<IActionResult> CreateNewTournament([FromBody] NewTournamentRequest newTournamentRequest)
+        {
+            try
+            {
+                var tournamentId = await _tournamentService.CreateNewTournament(newTournamentRequest);
+                return Ok(tournamentId);
+            }
+            catch(MissingSubscriptionException)
+            {
+                return Unauthorized("A subscription is required for tournament creation");
+            }
+        }
+    }
+}
