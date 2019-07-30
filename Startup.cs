ba10ef825e909b51351cd83cf0928184c835f99f
Ole J. Skogstad <skogstad@protonmail.com>
2019-07-30 18:06:16 +0200

    Attempt to fix CORS
    Will see how it works on linode

--- Startup.cs
+++ Startup.cs
@@ -8,6 +8,7 @@ using Isolani.Services;
 using Isolani.Services.Interfaces;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Cors.Infrastructure;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
@@ -25,20 +26,31 @@ namespace Isolani
         }
 
         private IConfiguration Configuration { get; }
-        
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            // ********************
+            // Setup CORS
+            // ********************
+            var corsBuilder = new CorsPolicyBuilder();
+            corsBuilder.AllowAnyOrigin(); 
+            corsBuilder.AllowAnyHeader();
+            corsBuilder.AllowAnyMethod();
+
+            services.AddCors(options =>
+            {
+                options.AddPolicy("SiteCorsPolicy", corsBuilder.Build());
+            });
+            
             services.AddTransient<IAuthenticationService, AuthenticationService>();
             services.AddTransient<IUserManagementService, UserManagementService>();
             services.AddTransient<ITournamentService, TournamentService>();
-            
+
             var tokenSettingsSection = Configuration.GetSection("TokenSettings");
             services.Configure<TokenSettings>(tokenSettingsSection);
 
-            services.AddCors();
-            
             services
                 .AddAuthentication(authenticationOptions =>
                 {
@@ -47,27 +59,27 @@ namespace Isolani
                 })
                 .AddJwtBearer(jwtBearerOptions =>
                 {
-                    var tokenSettings = tokenSettingsSection.Get<TokenSettings>();    
+                    var tokenSettings = tokenSettingsSection.Get<TokenSettings>();
                     jwtBearerOptions.SaveToken = false;
-                    
+
                     #if DEBUG
                     jwtBearerOptions.RequireHttpsMetadata = false;
                     #endif
-                    
+
                     jwtBearerOptions.TokenValidationParameters = new TokenValidationParameters
                     {
                         ValidateIssuerSigningKey = true,
                         ValidateLifetime = true,
-                        IssuerSigningKey =  new SymmetricSecurityKey(Encoding.ASCII.GetBytes(tokenSettings.Secret)),
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(tokenSettings.Secret)),
                         ValidIssuer = tokenSettings.Issuer,
                         ValidateIssuer = false,
                         ValidateAudience = false,
                         ClockSkew = TimeSpan.Zero
                     };
                 });
-            
+
             services
-                .AddDbContext<IsolaniDbContext>(options => 
+                .AddDbContext<IsolaniDbContext>(options =>
                 {
                     options.UseNpgsql(Configuration.GetConnectionString("DefaultConnection"));
                 })
@@ -85,6 +97,8 @@ namespace Isolani
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            app.UseCors("SiteCorsPolicy");
+            
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
@@ -93,18 +107,10 @@ namespace Isolani
             {
                 app.UseHsts();
             }
-
-            app.UseCors(corsPolicyBuilder =>
-                corsPolicyBuilder
-                    .AllowAnyOrigin()
-                    .AllowAnyMethod()
-                    .AllowAnyHeader()
-                    .WithExposedHeaders("Token-Expired")
-                    .AllowCredentials()
-                    .Build())
-                .UseAuthentication()
-                .UseHttpsRedirection()
-                .UseMvc();
+            
+            app.UseHttpsRedirection();
+            app.UseAuthentication();
+            app.UseMvc();
         }
     }
 }
